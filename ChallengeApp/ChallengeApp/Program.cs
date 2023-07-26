ae0f4621b2473cb74c9b045a8a739c5db369ed07
Piotr Dyduch <padyduch@o2.pl>
2023-07-26 12:17:46 +0200

    Zadanie dzień 5

--- ChallengeApp/ChallengeApp/Program.cs
+++ ChallengeApp/ChallengeApp/Program.cs
@@ -1,20 +1,59 @@
-﻿int age = 13;
-string name = "Marian";
-bool isWoman = false;
+﻿int number = 546879879;
+string numberAsString = number.ToString();
+char[] letters = numberAsString.ToCharArray();
+int[] counter = {0, 0, 0, 0, 0, 0, 0, 0, 0, 0};
 
-if (age < 30 && isWoman)
+foreach (char letter in letters)
 {
-    Console.WriteLine("Kobieta poniżej 30 lat");
+    if (letter == '0')
+    {
+        counter[0]++;
+    }
+    else if (letter == '1')
+    {
+        counter[1]++;
+    }
+    else if (letter == '2')
+    {
+        counter[2]++;
+    }
+    else if (letter == '3')
+    {
+        counter[3]++;
+    }
+    else if (letter == '4')
+    {
+        counter[4]++;
+    }
+    else if (letter == '5')
+    {
+        counter[5]++;
+    }
+    else if (letter == '6')
+    {
+        counter[6]++;
+    }
+    else if (letter == '7')
+    {
+        counter[7]++;
+    }
+    else if (letter == '8')
+    {
+        counter[8]++;
+    }
+    else if (letter == '9')
+    {
+        counter[9]++;
+    }
+    //Console.WriteLine(counter[5]);
 }
-else if (name == "Ewa" && age == 33)
-{
-    Console.WriteLine("Ewa, lat 33");
-}
-else if (!isWoman && age < 18)
-{
-    Console.WriteLine("Mężczyzna niepełnoletni");
-}
-else
-{
-    Console.WriteLine("każda inna kobieta lub każdy inny mężczyzna");
-}
+Console.WriteLine("0 =>" + counter[0]);
+Console.WriteLine("1 =>" + counter[1]);
+Console.WriteLine("2 =>" + counter[2]);
+Console.WriteLine("3 =>" + counter[3]);
+Console.WriteLine("4 =>" + counter[4]);
+Console.WriteLine("5 =>" + counter[5]);
+Console.WriteLine("6 =>" + counter[6]);
+Console.WriteLine("7 =>" + counter[7]);
+Console.WriteLine("8 =>" + counter[8]);
+Console.WriteLine("9 =>" + counter[9]);

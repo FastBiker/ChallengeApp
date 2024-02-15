e5e4cc3f7494fb9cf7fc80bf6503a15a90723dd2
Piotr Dyduch <padyduch@o2.pl>
2024-02-15 18:35:25 +0100

    Day 19

--- ChallengeApp/ChallengeApp/Person.cs
+++ ChallengeApp/ChallengeApp/Person.cs
@@ -7,6 +7,7 @@
             this.Name = name;
             this.Surname = surname;
             this.Sex = sex;
+            this.Age = age;
         }
         public string Name { get; private set; }
         public string Surname { get; private set; }
